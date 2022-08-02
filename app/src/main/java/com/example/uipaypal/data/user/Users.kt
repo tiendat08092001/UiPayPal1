761115e45bbd607e93ae26c8283ece4f63031094
DatNguyen <tiendat08092001@gmail.com>
2022-08-02 10:30:03 +0700

    fix UI

--- app/src/main/java/com/example/uipaypal/data/user/Users.kt
+++ app/src/main/java/com/example/uipaypal/data/user/Users.kt
@@ -7,6 +7,6 @@ fun userList(): List<User> {
         User(id = 1, name = "Carlos Roca", image = R.drawable.user_image2),
         User(id = 2, name = "Ruby Sanz", image = R.drawable.user_image3),
         User(id = 3, name = "Mary Rich", image = R.drawable.user_image1),
-        User(id = 4, name = "Carlos Roca", image = R.drawable.user_image4),
+        User(id = 4, name = "José Porto", image = R.drawable.user_image4),
     )
 }
